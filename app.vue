86615551693eb5f50530087a67d8211c811761fa
Sooraj P R <19621059+soorajiam@users.noreply.github.com>
2024-04-23 23:52:25 +0100

    GTM

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <Head>
     <Meta name="google-adsense-account" content="ca-pub-9324362451727622" />
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9324362451727622"
+     crossorigin="anonymous" />
   </Head>
   <div class="bg-white dark:bg-gray-800">
     <NuxtLayout>
